a98b9887afc01a5ebb6d7d01f57438cdbd054e44
tauseef-khan <tdakhan@gmail.com>
2017-05-20 23:29:05 +0100

    multiple components implemented

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+// required imports
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+
+
+class App extends React.Component {
+	render() {
+		//return what needs to be rendered
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Home/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+}
+
+render(<App/>, window.document.getElementById("app"));
