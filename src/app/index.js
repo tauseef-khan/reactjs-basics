11b96b78e3687e53f1c99912114b0a626889fe65
tauseef-khan <tdakhan@gmail.com>
2017-05-21 17:20:01 +0100

    Index.js with props implemented

--- src/app/index.js
+++ src/app/index.js
@@ -4,11 +4,17 @@ import { render } from "react-dom";
 
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
+import { Footer } from "./components/Footer";
 
 
 
 class App extends React.Component {
 	render() {
+		var user = {
+			name: "TK",
+			hobbies: ["Sports","Baking"]
+		};
+
 		//return what needs to be rendered
 		return(
 			<div className="container">
@@ -19,7 +25,14 @@ class App extends React.Component {
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home/>
+						<Home name={"Bob"} age={100} user={user}>
+							<p>This is a paragraph demonstrates passing of information using props INDIRECTLY</p>
+						</Home>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Footer/>
 					</div>
 				</div>
 			</div>
