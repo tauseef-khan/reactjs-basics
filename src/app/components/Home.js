1d857ce78859fdf33cd04d599f72d197d6c554c3
tauseef-khan <tdakhan@gmail.com>
2017-05-21 17:21:02 +0100

    Home.js with props (direct and indirect use)

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,7 +5,25 @@ export class Home extends React.Component {
 		return (
 			<div>
 				<p>In a new component</p>
+				{ 2 + 2 }
+				<p>Your name is {this.props.name}, your age is {this.props.age}</p>
+				<p>User Object => Name: {this.props.user.name}</p>
+				<div>
+					<h4>Hobbies</h4>
+					<ul>
+						{this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+					</ul>
+				</div>
+				<hr/>
+				{this.props.children}
 			</div>
 		);
 	}
 }
+
+Home.propTypes = {
+	name: React.PropTypes.string,
+	age: React.PropTypes.number,
+	user: React.PropTypes.object,
+	children: React.PropTypes.element.isRequired
+};
