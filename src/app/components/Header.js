ce358e2ebb80ed69c6c358962684c8eac97baa0e
tauseef-khan <tdakhan@gmail.com>
2017-05-24 20:44:46 +0100

    how DOM reacts and stateless componenets

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-export class Header extends React.Component {
-	render() {
+export const Header = (props) => {
 		return(
 			<nav className="navbar navbar-default">
 				<div className="container">
 					<div className="navbar-header">
 						<ul className="nav navbar-nav">
-							<li><a href="#">Home</a></li>
+							<li><a href="#">{props.homeLink}</a></li>
 						</ul>
 					</div>
 				</div>
 			</nav>
-		)
-	}
-}
+		);
+};
